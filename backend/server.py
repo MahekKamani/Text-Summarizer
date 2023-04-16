e3a9b927147945c256ef94952d7e155f9b7fac4f
Mahek Kamani <72488411+MahekKamani@users.noreply.github.com>
2023-04-16 17:09:01 +0530

    Add files via upload

---
+++ backend/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from utils import get_currdate_hash
+from summary import generate_summary, generate_summary_file
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+
+    file = request.files['file']
+    unique_filename = f'{get_currdate_hash()}_{file.filename}'
+    file.save(f'tmp/{unique_filename}')
+    summary = generate_summary_file(f'tmp/{unique_filename}')
+    return jsonify({'summary': summary}), 200
+
+@app.route('/text', methods=['POST'])
+def upload_text():
+    text = request.get_json()['text']
+    if not text:
+        return 'No text provided', 400
+    
+    summary = generate_summary(text)
+    return jsonify({'summary': summary}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
